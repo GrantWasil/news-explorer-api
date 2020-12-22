bd72761163d50df978e26011be542df40a0f6c57
Grant Wasil <dev@grantwasil.com>
2020-12-22 04:15:23 -0700

    feat: added user auth routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,41 @@
+const { JWT_SECRET } = process.env;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 module.exports.getCurrentUser = (req, res) => {
   res.send({
     data: req.user,
   });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    email,
+    password,
+    name,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+    }))
+    .then((user) => res.status(201).send({
+      data: {
+        name: user.name,
+        email: user.email,
+      },
+    }))
+    .catch(next);
+};
+
+module.exports.loginUser = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch(next);
 };
