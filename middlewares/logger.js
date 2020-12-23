756364badcd04ada17d90879d1b0e7298b7c0f92
Grant Wasil <dev@grantwasil.com>
2020-12-22 22:13:24 -0700

    fix: moved logging to new directory

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,16 +1,23 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const fs = require('fs');
+const path = require('path');
+
+const logDirectory = 'logs';
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: path.join(logDirectory, 'request.log') }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: path.join(logDirectory, 'error.log') }),
   ],
   format: winston.format.json(),
 });
