aab7400696ac92000dbf13dbe9633a5ee064c46e
Grant Wasil <dev@grantwasil.com>
2020-12-22 23:01:10 -0700

    feat: added celebrate filtering in routes

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const articlesRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getArticles,
@@ -6,8 +7,34 @@ const {
   deleteArticle,
 } = require('../controllers/articles');
 
-articlesRouter.get('/', getArticles);
-articlesRouter.post('/', createArticle);
-articlesRouter.delete('/:articleId', deleteArticle);
+articlesRouter.get('/', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), getArticles);
+
+articlesRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required(),
+    image: Joi.string().required(),
+  }),
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), createArticle);
+
+articlesRouter.delete('/:articleId', celebrate({
+  params: Joi.object().keys({
+    articleId: Joi.string().hex().length(24).required(),
+  }),
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), deleteArticle);
 
 module.exports = articlesRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 const usersRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getCurrentUser,
 } = require('../controllers/users');
 
-usersRouter.get('/me', getCurrentUser);
+usersRouter.get('/me', celebrate({
+  params: Joi.object.keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), getCurrentUser);
 
 module.exports = usersRouter;
